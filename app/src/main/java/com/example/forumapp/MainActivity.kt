60d86876a951cfb20e0fa8fcf25340885c157fe7
SamuelLimaCap <samueldelimapessoal@gmail.com>
2022-02-05 16:58:30 -0300

    Added new Layout to show when user don't have connection

--- app/src/main/java/com/example/forumapp/MainActivity.kt
+++ app/src/main/java/com/example/forumapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.View
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -13,7 +14,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.forumapp.adapters.PostAdapter
 import com.example.forumapp.databinding.ActivityMainBinding
+import com.example.forumapp.models.enum.EnumResponse
 import com.example.forumapp.viewmodels.PostListViewModel
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,8 +34,12 @@ class MainActivity : AppCompatActivity() {
         init()
         setupRecyclerView()
         setupLiveDataObserver()
-        if (postListViewModel.postList.value!!.isEmpty()) postListViewModel.getPosts()
+        setupErrorButtonListener()
+        if (postListViewModel.postList.value!!.response == EnumResponse.ERROR) {
+            getPosts()
+        }
     }
+
 
     private fun init() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -40,7 +47,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupRecyclerView() {
-        postAdapter.addData(postListViewModel.postList.value!!)
+        postAdapter.setData(postListViewModel.postList.value!!.data)
         binding.rvPost.adapter = postAdapter
         binding.rvPost.layoutManager = LinearLayoutManager(this)
         binding.rvPost.addOnScrollListener(onScrollListener)
@@ -49,24 +56,37 @@ class MainActivity : AppCompatActivity() {
     private fun setupLiveDataObserver() {
         postListViewModel.postList.observe(this, Observer {
             binding.loadingPostItems.visibility = View.VISIBLE
-            postAdapter.setData(it)
+            if (it.response == EnumResponse.DONE) {
+                postAdapter.setData(it.data)
+            } else {
+                binding.errorMessage.visibility = View.VISIBLE
+            }
             binding.loadingPostItems.visibility = View.GONE
             isLoading = false
             wasOnBottom = false
-
-
         })
+    }
+
+    private fun setupErrorButtonListener() {
+        binding.buttonErrorMessage.setOnClickListener {
+            getPosts()
+        }
+    }
+
+    private fun getPosts() {
+        binding.errorMessage.visibility = View.GONE
+        binding.loadingPostItems.visibility = View.VISIBLE
+        postListViewModel.getPosts()
     }
 
     private val onScrollListener = object : RecyclerView.OnScrollListener() {
         override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
             super.onScrollStateChanged(recyclerView, newState)
-
             if (!recyclerView.canScrollVertically(1)) {
                 if (!wasOnBottom && !isLoading) {
                     wasOnBottom = true
                     isLoading = true
-                    postListViewModel.getPosts()
+                    getPosts()
                 }
             }
         }
